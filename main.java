62a38618d9b66e78c28420034074991b63a007cb
max-ellis <mjellis@ualberta.ca>
2019-11-23 15:46:37 -0700

    squared x

---
+++ main.java
@@ -0,0 +1,27 @@
+
+
+public class main {
+
+    public static void main(String args[]){
+        foo();
+    }
+
+    private static void foo(){
+        int x = getIntX();
+        int y = getIntY();
+	x = x*x;
+        graph(x, y);
+    }
+
+    private static int getIntX(){
+        return 5;
+    }
+
+    private static int getIntY(){
+        return 4;
+    }
+
+    private static void graph(int a, int b){
+        System.out.println(a*b);
+    }
+}
