dd9e4242f2c71cacf9db78111067a2d6aa56211a
max-ellis <mjellis@ualberta.ca>
2020-02-05 12:15:12 -0700

    modified example

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
       Parent1 parent1 = new Parent1();
       Child1 child1 = new Child1();
       int x = parent1.getNum();
-      int y = child1.getVal();
+      int y = child1.test();
       System.out.println(x + "  " + y);
     }
 
@@ -31,8 +31,8 @@ class Parent1 {
 
 class Child1 extends Parent1 {
 
-  public int getVal() {
-    return 50;
+  public int test() {
+    return getNum();
   } 
 
 }
