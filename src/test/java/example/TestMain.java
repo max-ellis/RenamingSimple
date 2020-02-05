ce4b08b2cc389bc0d24950355cb6f53457b6f2d5
max-ellis <mjellis@ualberta.ca>
2020-02-05 12:10:15 -0700

    rename getNum to getInt

--- src/test/java/example/TestMain.java
+++ src/test/java/example/TestMain.java
@@ -23,7 +23,7 @@ public class TestMain {
   @Test
   public void assertIntTest() {
     Parent1 parent1 = new Parent1();
-    int x = parent1.getNum();
+    int x = parent1.getInt();
     assertEquals(x, 50);
   }
 
