ea5c1ea184d9dea691f80f0d22a977bfa3e4ba37
max-ellis <mjellis@ualberta.ca>
2020-02-27 11:01:57 -0700

    rename method foo -> foobar

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 class A {
 
-  void foo() {
+  void foobar() {
 	int x = 0;
 	int y = 0;
 	int z = x + y;
@@ -25,14 +25,14 @@ class A {
 
 class B extends A {
   void originalBar() {
-	foo();
+	foobar();
   }
 
 }
 
 class C extends A {
   void originalBar() {
-    foo();
+    foobar();
     int a = 0;
 	int b = 1;
 	int c = 2;
@@ -44,7 +44,7 @@ class C extends A {
 
 class D extends A {
   void originalBar() {
-    foo();
+    foobar();
     int a = 0;
 	int b = 1;
 	int c = 2;
