432912525ffca47682a0b726a717e841055118e6
max-ellis <mjellis@ualberta.ca>
2020-02-27 09:19:47 -0700

    rename class A to newA

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 }
 
-class A {
+class newA {
 
   void foo() {
 	int x = 0;
@@ -18,7 +18,7 @@ class A {
 }
 
 
-class B extends A {
+class B extends newA {
   void bar() {
 	foo();
   }
