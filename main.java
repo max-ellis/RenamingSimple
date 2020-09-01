773ef6b34bcbb0d5c26eaa89c1776e4450572adf
Max Ellis <mjellis@ualberta.ca>
2020-09-01 17:56:21 -0600

    reset for method override matrix test

--- main.java
+++ main.java
@@ -17,20 +17,33 @@ public class main {
     }
 }
 
-class exampleClass1 {
+class superClass {
 
-    void foobar() {
+    void foo() {
 	String code = "Code here";
 	System.out.println(code);
     }
 
-    void exampleMethodFoo1() {
+    void bar() {
 	int x = 5;
 	int y = 10;
 	for(int z = 0; z < x; z++) {
 		System.out.println(y * z);
 	}
-	System.out.println("This is a rename class and method example");
+	System.out.println("This is a rename method override example");
     }
 
+}
+
+class subClass extends superClass {
+
+    void foo() {
+	String code = "subclass code here";
+	System.out.println(code);
+    }
+
+    void method() {
+	String words = "This is in no way related to bar above";
+	System.out.println(words);
+    }
 }
