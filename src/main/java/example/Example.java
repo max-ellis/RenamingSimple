c224383c64d91ab0b94a7e7261edb75b84a26e4f
Max Ellis <mjellis@ualberta.ca>
2021-03-09 17:40:39 -0800

    Rename Main -> Example and Example.getNumberOfStudents to Example.newMethodName

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -1,17 +1,17 @@
 package example;
 
 
-public class Main {
+public class Example {
 
     public static void main(String args[]){
 	System.out.println("Example for nested refactorings across commits");
 	String name = "RefMerge University";
 	Place university = new Place(name, 50);
-	int students = Main.getNumberOfStudents(university);
+	int students = Example.newMethodName(university);
 
     }
 
-    public static int getNumberOfStudents(Place school) {
+    public static int newMethodName(Place school) {
 	System.out.println("Main will be renamed in the first commit and this will be renamed to getStudents in the third commit");
 	return school.getNumberOfStudents();
     }
